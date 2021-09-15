4d51b662468733ff0c3e939c2cc3e27280928632
UserRamis <90569354+UserRamis@users.noreply.github.com>
2021-09-15 21:11:15 +0300

    Add files via upload

---
+++ Laboratory Works/Laboratory 2.1/Laboratory 2.1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratory_2._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите русскую букву ");
+            string bukva = Convert.ToString(Console.ReadLine());
+            List<string> alphabet = new List<string> { "а", "б", "в", "г", "д", "е", "ё", "ж", "з", "и", "й", "к", "л", "м", "н", "о", "п", "р", "с", "т", "у", "ф", "х", "ц", "ч", "ш", "щ", "ъ", "ы", "ь", "э", "ю", "я" };
+            var index= alphabet.IndexOf(bukva);
+            if (index !=32)
+            {
+                var nextSymbol = alphabet[index + 1];
+                Console.WriteLine("Следующая буква по алфавиту: " + nextSymbol);
+            }
+            else
+            {
+                Console.WriteLine("Тогда следующая буква будет а");
+            }
+            Console.ReadKey();
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
