7390b5a8a6cf063ad25e849928c7d210a6805406
UserRamis <90569354+UserRamis@users.noreply.github.com>
2021-09-15 18:40:25 +0300

    Add files via upload

---
+++ Laboratory Works/Laboratory work 1.2/Laboratory work 1.2/1.2.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratory_work_1._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите первое число ");
+            double chislo1 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Введите второе число ");
+            double chislo2 = Convert.ToDouble(Console.ReadLine());
+            if (chislo2 = 0) ;
+
+
+
+        }
+    }
+}
