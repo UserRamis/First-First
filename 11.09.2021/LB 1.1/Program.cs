5983b14395c1856889da20ee786c5b00ef31a799
UserRamis <90569354+UserRamis@users.noreply.github.com>
2021-09-24 20:54:01 +0300

    Add files via upload

---
+++ 11.09.2021/LB 1.1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LB_1._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите ваше имя ");
+            string name = Convert.ToString(Console.ReadLine());
+            Console.WriteLine("Приветствуем вас, " + name + "!");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 11.09.2021/LB2.2/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LB2._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите коэффицент а ");
+            double coefficientA, coefficientB, coefficientC, disriminant, x1, x2, x3;
+            coefficientA = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Введите коэффицент b ");
+            coefficientB = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Введите коэффицент c ");
+            coefficientC = Convert.ToDouble(Console.ReadLine());
+            disriminant = (Math.Round(Math.Pow(coefficientB, 2) - 4 * coefficientA * coefficientC, 2));
+            if (disriminant > 0)
+            {
+                x1 = ((-coefficientB + Math.Sqrt(disriminant)) / 2 * coefficientA);
+                x2 = ((-coefficientB - Math.Sqrt(disriminant)) / 2 * coefficientA);
+                Console.WriteLine("Уравнение имеет два корня" + "\nтак x1=" + Math.Round(x1, 2) + "\nx2=" + Math.Round(x2, 2));
+            }
+            else
+            {
+                if (disriminant == 0)
+                {
+                    x3 = Math.Round(-coefficientB / (2 * coefficientA), 2);
+                    Console.WriteLine("Уравнение имеет один корень " + x3);
+                }
+
+                else
+                {
+                    Console.WriteLine("Уравнение не имеет решения");
+                }
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
